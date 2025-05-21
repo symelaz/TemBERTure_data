b40895ab67d0c8bc538d648c105df82a8a4513ff
Symela Lazaridi <94408242+symelaz@users.noreply.github.com>
2025-05-21 14:58:20 +0200

    Update create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -1,5 +1,24 @@
+################ Inputs ################
+# 1 --> Name of the conda environment to create
+# 2 --> 
+
 conda create -n DATASET
 conda activate DATASET
 
 conda install conda-forge::biopython
 pip install tqdm pandas networkx matplotlib
+
+# Installing mmseqs from source 
+wget https://mmseqs.com/latest/mmseqs-linux-avx2.tar.gz
+tar xvfz mmseqs-linux-avx2.tar.gz
+export PATH=$(pwd)/mmseqs/bin/:$PATH
+
+# Installing hmmer
+wget http://eddylab.org/software/hmmer/hmmer.tar.gz 
+tar zxf hmmer.tar.gz
+cd hmmer-3.3.2
+./configure --prefix /your/install/path
+make
+make check
+make install
+
